31ca24a73e54fbe394520f1789c6e8e3ab8b411b
E-Spin-lab <143534565+E-Spin-lab@users.noreply.github.com>
2024-12-23 15:43:44 -0500

    Upload

---
+++ Capripox_Kraken_batch.sh
@@ -0,0 +1,89 @@
+CapripoxDirectory="/lustrefs/path/to/project/"
+ExtractReadDirectory="/lustrefs/path/to/project/Illumina/directories/"
+DB="/lustrefs/path/to/Kraken/minikraken_20171101_8GB_dustmasked"
+FileLocations="MetadataNew_Master.csv"
+
+## Capripox Taxonomic ID
+Capripox________Parent="10265"
+
+tail -n +2 $CapripoxDirectory$FileLocations | while read LINE; do
+    
+    ## Parsing CSV for metadata
+    Virus="$(cut -d',' -f2 <<< $LINE)"
+    Sample="$(cut -d',' -f9 <<< $LINE)"
+    Read01=$CapripoxDirectory"$(cut -d',' -f10 <<< $LINE)"
+    Read02=$CapripoxDirectory"$(cut -d',' -f11 <<< $LINE)"
+    Read03=$CapripoxDirectory"$(cut -d',' -f12 <<< $LINE)"
+    Read04=$CapripoxDirectory"$(cut -d',' -f13 <<< $LINE)"
+
+    ## Set Kraken output file names
+    paired_01_kraken=$DB"/Capripox_Batch/"$Sample"_output.kraken"
+    paired_02_kraken=$DB"/Capripox_Batch/"$Sample"_output_2.kraken"
+
+    ## Set Extracted Paired-Read File Names
+    Classified_read_01=$CapripoxDirectory$ExtractReadDirectory$Sample"_classified_Capri_R1.fastq"
+    Classified_read_02=$CapripoxDirectory$ExtractReadDirectory$Sample"_classified_Capri_R2.fastq"
+    Classified_read_03=$CapripoxDirectory$ExtractReadDirectory$Sample"_classified_Capri_R3.fastq"
+    Classified_read_04=$CapripoxDirectory$ExtractReadDirectory$Sample"_classified_Capri_R4.fastq"
+
+    taxid=$Capripox________Parent
+
+    echo $Virus
+    echo $Sample
+    echo $taxid
+    echo ""
+    echo $Read01
+    echo $Read02
+    echo $Read03
+    echo $Read04
+    echo "#########################################"
+    
+    # Create batch file 01
+    echo \
+"#!/usr/bin/env bash
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --partition=cpu_compute
+#SBATCH --cpus-per-task=96
+#SBATCH --job-name=${Sample}
+#SBATCH --time=7-10:00:00
+#SBATCH --output=${Sample}_capri_01.log
+
+module load kraken
+module load kraken_tools
+
+kraken --fastq-input ${Read01} ${Read02} --paired --gzip-compressed --db ${DB} --threads 95 --only-classified-output --out-fmt interleaved> ${paired_01_kraken}
+kraken-report --db ${DB} ${paired_01_kraken} > ${paired_01_kraken}_report.txt
+extract_kraken_reads.py -k ${paired_01_kraken} -s ${Read01} -s2 ${Read02} -t ${taxid} -r ${paired_01_kraken}_report.txt --fastq-output -o ${Classified_read_01} -o2 ${Classified_read_02} --include-children
+gzip ${Classified_read_01}
+gzip ${Classified_read_02}"\
+    > $Sample"_capri_01.sh"
+    
+        # Create batch file 02
+        echo \
+"#!/usr/bin/env bash
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --partition=cpu_compute
+#SBATCH --cpus-per-task=96
+#SBATCH --job-name=${Sample}
+#SBATCH --time=7-10:00:00
+#SBATCH --output=${Sample}_capri_02.log
+
+module load kraken
+module load kraken_tools
+
+kraken --fastq-input ${Read03} ${Read04} --paired --gzip-compressed --db ${DB} --threads 95 --only-classified-output --out-fmt interleaved> ${paired_02_kraken}
+kraken-report --db ${DB} ${paired_02_kraken} > ${paired_02_kraken}_report.txt
+extract_kraken_reads.py -k ${paired_02_kraken} -s ${Read03} -s2 ${Read04} -t ${taxid} -r ${paired_02_kraken}_report.txt --fastq-output -o ${Classified_read_03} -o2 ${Classified_read_04} --include-children
+gzip ${Classified_read_03}
+gzip ${Classified_read_04}"\
+    > $Sample"_capri_02.sh"
+
+    
+    # Run batch files
+    sbatch $Sample"_capri_01.sh"
+    sbatch $Sample"_capri_02.sh"
+
+done
+echo "Complete"
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# CapripoxKrakenBatch
+# Batch Illumina paired-end read taxonomic classification and extraction using Kraken.
+This repository features Kraken_Classify_Extract, a bash script intended for the assignment of taxonomic labels to Illumina paired-read files, followed by the extraction of all reads associated with a designated taxonomic ID. A metadata file contains the Sample ID, Virus species, and full path to sequence files. This data is processed to generate and execute a bash script for each individual sample that is intended to be executed on a High-Performance Computing (HPC) system.
+
+## Background
+Capripoxvirus is a genus of large DNA viruses. Several Capripox viruses (LSDV, GTPV, SPPV) samples were sequenced for  de novo assembly. This script was developed as an initial step to enhance the efficiency of subsequent analyses by removing host (and other potential contimant) derived reads. Following processing reads were processed using a previously developed and modified ASFV assembly Pipeline. https://github.com/Global-ASFV-Research-Alliance/ASFV_Pipeline
+
+## Requirements
+--Kraken
+
+--Kraken tools
+
+--minikraken_20171101_8GB_dustmasked database
+
+-- MetaData.csv
+
+The csv file will contain all metadata and files paths associated with each sample and will contain the following headers:
+
+*Project_ID, 
+Species collection_date country location host tissue_type collected_by isolate L1R1 L1R2 L2R1 L2R2 L3R1 L3R2 L4R1 L4R2 Nanopore_Directory*
+
+
+## Code
+
+Mark the file as executable (performed once) 
+
+    chmod +x Capripox_Kraken_batch.sh
+
+To run
+
+    ./Capripox_Kraken_batch.sh
+
+A bash script will be created for each Illumina paired-read and will be analyzed on its own HPC node.
+
+### Usage
+This script was designed for a particular experiment and not for deployment, and therefore, command-line arguments were not incorporated. Future applications involving different reference genomes and experiments would require modifications to lines 1-4 & 7.
